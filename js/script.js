50698834d03d70279f84a65aaefc5e7af67dc566
Emma Paris <eparis@atlassian.com>
2022-03-14 14:26:01 +0300

    update changes

--- js/script.js
+++ js/script.js
@@ -1,7 +1,12 @@
- jQuery(document).ready(function(){  
+$(document).ready(function(){  
   $(".clickable").click(function)() {
    $(one).toggle();
    $(two).toggle();
    $(three).toggle();
  });
+   $(".bold").bold(function){
+     $(one(h12)).bold();
+     $(two(h12)).bold();
+     $(three(h12)).bold();
+   });
  }); 
