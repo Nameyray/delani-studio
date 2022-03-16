fa550c8f83c27899c3058c13ca67228efc915aae
Emma Paris <eparis@atlassian.com>
2022-03-16 23:09:21 +0300

    updated toggle effect on the script file

--- js/script.js
+++ js/script.js
@@ -1,18 +1,34 @@
 $(document).ready(function(){  
-  $(".clickable").click(function()) {
+  $(".clickable").click(function(){
    $(".one").toggle();
    $(".two").toggle();
    $(".three").toggle();
- });
-   $(".bold").bold(function){
-     $(".one"(h12)).bold && center();
-     $(".two"(h12)).bold && center();
-     $(".three"(h12)).bold && center();
-   });
- }); 
- 
- let message = yourMessageHere();
- alert(Name + 'we have received your message. Thank you for reaching out to us');
+  });
+  $(".one").click(function(){
+    $("img#one").toggle();
+    $("p#p1").toggle();
+  })
+  $(".two").click(function(){
+    $("img#two").toggle();
+    $("p#p2").toggle();
+  })
+  $(".three").click(function(){
+    $("img#three").toggle();
+    $("p#p3").toggle();
+  })
+});
+$(document).ready(function(){
+  $("portfolio").hover(function(){
+    $(img)("background-color", "white");
+    }, function(){
+    $(img)("background-color", "white");
+  });
+});
+
+
+
+
+
 
 
 
