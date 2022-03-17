f975a29048ad1679b606d14063c78be48b86c0a8
Nameyray <eparis@atlassian.com>
2022-03-17 11:26:51 +0300

    updated style and hover effect on script file

--- js/script.js
+++ js/script.js
@@ -17,21 +17,39 @@ $(document).ready(function(){
     $("p#p3").toggle();
   })
 });
-$(document).ready(function(){
-  $("portfolio").hover(function(){
-    $(img)("background-color", "white");
-    }, function(){
-    $(img)("background-color", "white");
-  });
+
+
+$("#inputname"). submit(function(event){
+  alert(inputName + "we have received your message. Thank you for reaching out to us.")
+  event.preventDefault();
 });
-
-
-
-
-
-
-
-
-
-
- 
+  
+  
+    $(".work1").hover(function () {
+      $("#p1").toggle()
+    });
+    $(".work2").hover(function () {
+      $("#p2").toggle()
+    });
+    $(".work3").hover(function () {
+      $("#p3").toggle()
+    });
+    $(".work4").hover(function () {
+      $("#p4").toggle()
+    });
+    $(".work5").hover(function () {
+      $("#p5").toggle()
+    });
+    $(".work6").hover(function () {
+      $("#p6").toggle()
+    });
+    $(".work7").hover(function () {
+      $("#p7").toggle()
+    });
+    $(".work8").hover(function () {
+      $("#p8").toggle()
+    });
+  
+  
+  });
+  
